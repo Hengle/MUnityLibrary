519b238589bb59909732c592c2d66cbe112c7d1a
Masataka Kohagura <masalinkinpark@gmail.com>
2017-12-19 23:29:18 +0900

    依存先の削除

--- Test/Scene/Test.cs
+++ Test/Scene/Test.cs
@@ -1,20 +1,15 @@
-﻿using Flw.Scene;
-using UnityEngine;
+﻿using UnityEngine;
 using UnityEngine.UI;
 
-namespace Flw.Test.Scene
-{
-    public class Test : MonoBehaviour
-    {
+namespace Flw.Test.Scene {
+    public class Test : MonoBehaviour {
         [SerializeField] private Button _sceneAButton;
 
-        private void Start()
-        {
-            _sceneAButton.onClick.AddListener(OnClickedSceneAButton);
+        private void Start () {
+            _sceneAButton.onClick.AddListener (OnClickedSceneAButton);
         }
 
-        private static void OnClickedSceneAButton()
-        {
+        private static void OnClickedSceneAButton () {
             //SceneLoader.LoadScene(Scenes.TestB);
         }
     }
