7347782f5f5a696c70ef68d0e60c17b01e80c80f
Masataka Kohagura <masalinkinpark@gmail.com>
2018-02-19 11:28:15 +0900

    ItemMenu の順番を変更

--- Editor/ScriptTempleteForMvpPattern/NewScriptForMvp.cs
+++ Editor/ScriptTempleteForMvpPattern/NewScriptForMvp.cs
@@ -12,12 +12,13 @@ namespace MUnityLibrary.Editor.Script
 
         private const string CSharpScriptIconName = "cs Script Icon";
 
-        [MenuItem("Assets/Create/C# Script for MVP", false, 80)]
+        [MenuItem("Assets/Create/C# Script(MVP Pattern)", false, 81)]
         public static void CreateCSharpScriptForMvp()
         {
             var icon = EditorGUIUtility.IconContent(CSharpScriptIconName).image as Texture2D;
 
-            var endNameEditAction = ScriptableObject.CreateInstance<CreateScriptEndNameEditAction>() as EndNameEditAction;
+            var endNameEditAction =
+                ScriptableObject.CreateInstance<CreateScriptEndNameEditAction>() as EndNameEditAction;
 
             ProjectWindowUtil.StartNameEditingIfProjectWindowExists(0, endNameEditAction, DefaultAssetName, icon,
                 ResourcePathOfScriptTemplete);
